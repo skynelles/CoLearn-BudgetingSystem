5259ea80c96b3967ad721093eeb55adf5258852f
Joel Bouwkamp <siftd106@yahoo.com>
2013-05-14 19:04:38 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoLearnBase::Application.config.secret_token = 'f6d6a7150403c18fdb0cfc06db0d10f85de3cf7a386f0163e5fcb6eb2bd9048547e32be12933575970ee553b256f2cc3cf1058c74cbefd67c5c770759e9f737f'
